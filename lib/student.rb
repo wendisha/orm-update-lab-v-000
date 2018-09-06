8f546560d70e49a01b8d41129fbe197e179490c0
Wendy Calderon <wendycalderon@gmail.com>
2018-09-06 04:13:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,19 +46,11 @@ class Student
   end
   
   def self.new_from_db(row)
-<<<<<<< HEAD
     id = row[0]
     name = row[1]
     grade = row[2]
     self.new(id, name, grade) 
-=======
-    new_student = self.new 
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
->>>>>>> ee0bb2d674807ab92e63ddc66c885b239975c5e4
-  end
+end
 
   def self.find_by_name(name)
     sql = <<-SQL
